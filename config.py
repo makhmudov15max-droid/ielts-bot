f5c3e203a0424fa1179254a73e1fc809a82eb0e0
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-05 19:56:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import json
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SHEET_ID = os.getenv("SHEET_ID")
-GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS_JSON")
+GOOGLE_CREDENTIALS = json.loads(os.getenv("GOOGLE_CREDENTIALS_JSON"))
