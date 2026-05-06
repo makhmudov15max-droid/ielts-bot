24f98e4ed157cfe2e1e90bc053f1a3785374583c
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-07 01:18:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,7 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils import executor
 import logging
 import os
-
-from sheets import get_report
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,24 +10,13 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# Keyboard
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-keyboard.add(KeyboardButton("Daily report"))
-
-# START
-@dp.message_handler(commands=["start"])
-async def start_handler(message: types.Message):
+@dp.message_handler()
+async def echo(message: types.Message):
+    print("MESSAGE KELDI:", message.text)
+    
     await message.answer(
-        "✅ Bot ishlayapti",
-        reply_markup=keyboard
+        f"Siz yozdingiz: {message.text}"
     )
 
-# DAILY REPORT
-@dp.message_handler(lambda message: message.text == "Daily report")
-async def daily_handler(message: types.Message):
-    report = get_report()
-    await message.answer(report)
-
-# RUN
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
