af122e673077ed98042d2438ae8cd32f1b4a6b99
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-07 02:36:36 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 from aiogram.utils import executor
+
 import logging
 import os
 
-from sheets import get_report
+from sheets import (
+    get_report,
+    update_teacher_score
+)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,11 +22,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# Keyboard
-keyboard = ReplyKeyboardMarkup(
-    resize_keyboard=True
-)
+# MEMORY
+selected_teacher = {}
 
+# MAIN MENU
 keyboard = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
@@ -30,6 +38,7 @@ keyboard.row(
 # START
 @dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
+
     await message.answer(
         "✅ new version",
         reply_markup=keyboard
@@ -38,13 +47,109 @@ async def start_handler(message: types.Message):
 # DAILY REPORT
 @dp.message_handler(lambda message: message.text == "Daily report")
 async def daily_handler(message: types.Message):
+
     report = get_report()
+
     await message.answer(report)
+
+# TEACHERS BUTTON
+@dp.message_handler(lambda message: message.text == "Teachers")
+async def teacher_menu(message: types.Message):
+
+    markup = InlineKeyboardMarkup(row_width=2)
+
+    teachers = [
+        "Adkhambek I",
+        "Sardorbek K",
+        "Akhmadali T",
+        "Obidjon R",
+        "Otabek M",
+        "Ilkhom A",
+        "Sevinch I",
+        "Khurshid Kh",
+        "Nilufar K",
+        "Farangiz E"
+    ]
+
+    buttons = []
+
+    for teacher in teachers:
+
+        buttons.append(
+            InlineKeyboardButton(
+                teacher,
+                callback_data=f"teacher_{teacher}"
+            )
+        )
+
+    markup.add(*buttons)
+
+    await message.answer(
+        "👨‍🏫 Ustozni tanlang",
+        reply_markup=markup
+    )
+
+# TEACHER SELECT
+@dp.callback_query_handler(lambda c: c.data.startswith("teacher_"))
+async def teacher_selected(callback: types.CallbackQuery):
+
+    teacher_name = callback.data.replace("teacher_", "")
+
+    selected_teacher[callback.from_user.id] = teacher_name
+
+    markup = InlineKeyboardMarkup(row_width=2)
+
+    markup.row(
+        InlineKeyboardButton(
+            "8.5",
+            callback_data="score_8.5"
+        ),
+
+        InlineKeyboardButton(
+            "9.0",
+            callback_data="score_9.0"
+        )
+    )
+
+    await callback.message.answer(
+        f"{teacher_name} ustozning yangilangan bali nechchi? 🧐",
+        reply_markup=markup
+    )
+
+# SCORE SELECT
+@dp.callback_query_handler(lambda c: c.data.startswith("score_"))
+async def score_selected(callback: types.CallbackQuery):
+
+    score = callback.data.replace("score_", "")
+
+    teacher_name = selected_teacher.get(
+        callback.from_user.id
+    )
+
+    if not teacher_name:
+
+        await callback.message.answer(
+            "❌ Ustoz topilmadi"
+        )
+
+        return
+
+    update_teacher_score(
+        teacher_name,
+        score
+    )
+
+    await callback.message.answer(
+        f"✅ {teacher_name} ustozimizning IELTS natijasi yangilandi 🙂"
+    )
 
 # OTHER
 @dp.message_handler()
 async def other_handler(message: types.Message):
-    await message.answer("Tugmadan foydalaning 👇")
+
+    await message.answer(
+        "Tugmalardan foydalaning 👇"
+    )
 
 # RUN
 if __name__ == "__main__":
