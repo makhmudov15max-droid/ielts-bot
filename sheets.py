8163fe4f5189a026c7aa3f1760b9b34fee05050f
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-07 01:21:21 +0500

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -2,11 +2,15 @@ import gspread
 from config import SHEET_ID, GOOGLE_CREDENTIALS
 
 gc = gspread.service_account_from_dict(GOOGLE_CREDENTIALS)
+
 sheet = gc.open_by_key(SHEET_ID).sheet1
+
 
 def get_report():
     try:
-        value = sheet.acell("I15").value
+        value = sheet.get("I15", value_render_option="FORMATTED_VALUE")[0][0]
+
         return f"📊 I15:\n{value}"
+
     except Exception as e:
         return f"❌ Error:\n{e}"
