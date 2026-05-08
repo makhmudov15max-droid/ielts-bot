53fb3868d64f628279e2c6896bfc567f987a0c1a
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-08 22:32:53 +0500

    Update states.py

--- states.py
+++ states.py
@@ -4,3 +4,4 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 class SalaryStates(StatesGroup):
     waiting_for_name = State()
     waiting_for_status = State()
+    waiting_for_hours = State()
