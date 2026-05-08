fb8a26e9d25b2aa37e8ff9cc3b58e16d16d0ec59
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 02:13:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -405,38 +405,38 @@ async def get_cover_hours(message: types.Message, state: FSMContext):
 
     individual_percentage = (actual_sales / individual_plan) * 100
 
-    if individual_percentage <= 49:
+        if individual_percentage <= 49:
         bonus_rate = 0
 
     elif individual_percentage <= 60:
-    bonus_rate = 5000
+        bonus_rate = 5000
 
     elif individual_percentage <= 70:
-    bonus_rate = 6000
+        bonus_rate = 6000
 
     elif individual_percentage <= 80:
-    bonus_rate = 10000
+        bonus_rate = 10000
 
     elif individual_percentage <= 90:
-    bonus_rate = 15000
+        bonus_rate = 15000
 
     elif individual_percentage <= 95:
-    bonus_rate = 18000
+        bonus_rate = 18000
 
     elif individual_percentage <= 100:
-    bonus_rate = 25000
+        bonus_rate = 25000
 
     elif individual_percentage <= 110:
-    bonus_rate = 30000
+        bonus_rate = 30000
 
     elif individual_percentage <= 120:
-    bonus_rate = 32000
+        bonus_rate = 32000
 
     elif individual_percentage <= 130:
-    bonus_rate = 35000
+        bonus_rate = 35000
 
     else:
-    bonus_rate = 40000
+        bonus_rate = 40000
 
     kpi_bonus = actual_sales * bonus_rate
 
