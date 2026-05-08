3addb0dfe515fcaa7fe6f5bf4af9fb6e0b9565c0
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 02:09:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -408,37 +408,37 @@ async def get_cover_hours(message: types.Message, state: FSMContext):
     if individual_percentage <= 49:
         bonus_rate = 0
 
-elif individual_percentage <= 60:
-    bonus_rate = 5000
+        elif individual_percentage <= 60:
+        bonus_rate = 5000
 
-elif individual_percentage <= 70:
-    bonus_rate = 6000
+        elif individual_percentage <= 70:
+        bonus_rate = 6000
 
-elif individual_percentage <= 80:
-    bonus_rate = 10000
+        elif individual_percentage <= 80:
+        bonus_rate = 10000
 
-elif individual_percentage <= 90:
-    bonus_rate = 15000
+        elif individual_percentage <= 90:
+        bonus_rate = 15000
 
-elif individual_percentage <= 95:
-    bonus_rate = 18000
+        elif individual_percentage <= 95:
+        bonus_rate = 18000
 
-elif individual_percentage <= 100:
-    bonus_rate = 25000
+        elif individual_percentage <= 100:
+        bonus_rate = 25000
 
-elif individual_percentage <= 110:
-    bonus_rate = 30000
+        elif individual_percentage <= 110:
+        bonus_rate = 30000
 
-elif individual_percentage <= 120:
-    bonus_rate = 32000
+        elif individual_percentage <= 120:
+        bonus_rate = 32000
 
-elif individual_percentage <= 130:
-    bonus_rate = 35000
+        elif individual_percentage <= 130:
+        bonus_rate = 35000
 
-else:
-    bonus_rate = 40000
+        else:
+        bonus_rate = 40000
 
-kpi_bonus = actual_sales * bonus_rate
+        kpi_bonus = actual_sales * bonus_rate
 
     await message.answer(f"📈 Individual KPI: {individual_percentage:.1f}%\n\n"
                          f"🔥 KPI Bonus: {kpi_bonus:,} UZS\n\n"
