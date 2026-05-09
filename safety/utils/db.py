33c806b7aa688832a452a8b746552f7a9a1a4b18
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 02:40:10 +0500

    Update db.py

--- safety/utils/db.py
+++ safety/utils/db.py
@@ -1,1 +1,21 @@
+import json
 
+
+def load_users():
+    with open("database/users.json", "r") as file:
+        return json.load(file)
+
+
+def save_users(data):
+    with open("database/users.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def load_pending():
+    with open("database/pending_users.json", "r") as file:
+        return json.load(file)
+
+
+def save_pending(data):
+    with open("database/pending_users.json", "w") as file:
+        json.dump(data, file, indent=4)
