25f78b945049d5060cba14edb41eac62bbe9be37
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 20:13:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from handlers.admin_salary import register_admin_handlers
 
 from config import TOKEN
+
+from handlers.admin_salary import register_admin_handlers
+
+from keyboards.admin_keyboard import main_menu_keyboard
+
 
 bot = Bot(token=TOKEN)
 
@@ -14,10 +18,14 @@ dp = Dispatcher(bot, storage=storage)
 @dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
 
-    await message.answer("Bot ishlayapti ✅")
+    await message.answer(
+        "Bot ishlayapti ✅",
+        reply_markup=main_menu_keyboard()
+    )
+
 
 register_admin_handlers(dp)
 
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-
