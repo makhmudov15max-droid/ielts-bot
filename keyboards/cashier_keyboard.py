c23d5c185efab3229012c442172ed2903962dc51
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 23:35:47 +0500

    Update cashier_keyboard.py

--- keyboards/cashier_keyboard.py
+++ keyboards/cashier_keyboard.py
@@ -1,1 +1,49 @@
+from aiogram.types import ReplyKeyboardMarkup
 
+
+def cashier_hours_keyboard():
+
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add("6 soat", "7 soat")
+    keyboard.add("8 soat", "9 soat")
+
+    keyboard.add("✍️ Boshqa")
+
+    keyboard.add("🏠 Bosh sahifa")
+
+    return keyboard
+
+
+def cashier_days_keyboard():
+
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add("24 kun", "25 kun")
+    keyboard.add("26 kun", "27 kun")
+
+    keyboard.add("✍️ Boshqa")
+
+    keyboard.add("🏠 Bosh sahifa")
+
+    return keyboard
+
+
+def yes_no_keyboard():
+
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add("✅ Ha", "❌ Yo'q")
+
+    keyboard.add("🏠 Bosh sahifa")
+
+    return keyboard
+
+
+def home_keyboard():
+
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add("🏠 Bosh sahifa")
+
+    return keyboard
