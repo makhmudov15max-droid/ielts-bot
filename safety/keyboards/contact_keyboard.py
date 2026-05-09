9fbbc838146e6a695fcc5d4b535addb7cd81d0fe
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 02:40:50 +0500

    Update contact_keyboard.py

--- safety/keyboards/contact_keyboard.py
+++ safety/keyboards/contact_keyboard.py
@@ -1,1 +1,13 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
+contact_keyboard = ReplyKeyboardMarkup(
+    resize_keyboard=True
+)
+
+contact_button = KeyboardButton(
+    text="📱 Raqam yuborish",
+    request_contact=True
+)
+
+contact_keyboard.add(contact_button)
