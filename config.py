2d5c9a0a291afdf522922549251d0179653ef7eb
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 19:53:52 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,1 @@
-import os
-import json
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SHEET_ID = os.getenv("SHEET_ID")
-
-GOOGLE_CREDENTIALS = json.loads(
-    os.getenv("GOOGLE_CREDENTIALS_JSON")
-)
+TOKEN = "8679587093:AAGjXpGVMiAexuNKPzRpQjASQRb8K2DYvyg"
