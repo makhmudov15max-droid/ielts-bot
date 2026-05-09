e0e78b146336b15bed34d305cfc5ffc64a595602
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 20:02:12 +0500

    Update admin_salary.py

--- handlers/admin_salary.py
+++ handlers/admin_salary.py
@@ -1,1 +1,46 @@
+from aiogram import types
+from aiogram.dispatcher import FSMContext
 
+from keyboards.admin_keyboard import (
+    main_menu_keyboard,
+    status_keyboard
+)
+
+from states.admin_states import AdminSalaryStates
+
+
+async def go_home(message: types.Message, state: FSMContext):
+
+    await state.finish()
+
+    await message.answer(
+        "🏠 Bosh sahifa",
+        reply_markup=main_menu_keyboard()
+    )
+
+
+def register_admin_handlers(dp):
+
+    @dp.message_handler(lambda message: message.text == "💰 Admin Salary")
+    async def salary_start(message: types.Message):
+
+        await message.answer(
+            "📋 Statusni tanlang:",
+            reply_markup=status_keyboard()
+        )
+
+        await AdminSalaryStates.waiting_for_status.set()
+
+
+    @dp.message_handler(state=AdminSalaryStates.waiting_for_status)
+    async def get_status(message: types.Message, state: FSMContext):
+
+        if message.text == "🏠 Bosh sahifa":
+            await go_home(message, state)
+            return
+
+        await state.update_data(status=message.text)
+
+        await message.answer(
+            f"✅ Tanlandi: {message.text}"
+        )
