954afd16e1495d6e7483fa8f337dbfb9ecae859f
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 20:12:58 +0500

    Update admin_keyboard.py

--- keyboards/admin_keyboard.py
+++ keyboards/admin_keyboard.py
@@ -11,39 +11,12 @@ def main_menu_keyboard():
     return keyboard
 
 
-
 def status_keyboard():
 
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 
     keyboard.add("Nova", "Prime")
     keyboard.add("Apex", "Leader")
-    keyboard.add("🏠 Bosh sahifa")
-
-    return keyboard
-
-
-
-def hours_keyboard():
-
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-
-    keyboard.add("6 soat", "7 soat")
-    keyboard.add("9 soat", "10 soat")
-    keyboard.add("✍️ Boshqa soat")
-    keyboard.add("🏠 Bosh sahifa")
-
-    return keyboard
-
-
-
-def days_keyboard():
-
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-
-    keyboard.add("24 kun", "25 kun")
-    keyboard.add("26 kun", "27 kun")
-    keyboard.add("✍️ Boshqa kun")
     keyboard.add("🏠 Bosh sahifa")
 
     return keyboard
