f2ee94a1d57dbba31c6abba57fd822d12b6fb7e0
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 20:31:39 +0500

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -1,27 +1,28 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-
 class AdminSalaryStates(StatesGroup):
 
-    waiting_for_status = State()
+```
+waiting_for_status = State()
 
-    waiting_for_hours = State()
-    waiting_for_days = State()
+waiting_for_hours = State()
+waiting_for_days = State()
 
-    waiting_for_individual_plan = State()
-    waiting_for_actual_sales = State()
+waiting_for_individual_plan = State()
+waiting_for_actual_sales = State()
 
-    waiting_for_conversion_plan = State()
-    waiting_for_actual_conversion = State()
+waiting_for_conversion_plan = State()
+waiting_for_actual_conversion = State()
 
-    waiting_for_active_plan = State()
-    waiting_for_actual_active = State()
+waiting_for_active_plan = State()
+waiting_for_actual_active = State()
 
-    waiting_for_russian = State()
-    waiting_for_ielts = State()
+waiting_for_russian = State()
+waiting_for_ielts = State()
 
-    waiting_for_missed_work = State()
-    waiting_for_missed_hours = State()
+waiting_for_missed_work = State()
+waiting_for_missed_hours = State()
 
-    waiting_for_cover = State()
-    waiting_for_cover_hours = State()
+waiting_for_cover = State()
+waiting_for_cover_hours = State()
+```
