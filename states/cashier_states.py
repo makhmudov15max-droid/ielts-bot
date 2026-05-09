574bb8ec3118d71705b263b21fb40d9ebb663bf2
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 23:35:10 +0500

    Update cashier_states.py

--- states/cashier_states.py
+++ states/cashier_states.py
@@ -1,1 +1,20 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
+
+class CashierSalaryStates(StatesGroup):
+
+    waiting_for_hours = State()
+
+    waiting_for_days = State()
+
+    waiting_for_missed_work = State()
+    waiting_for_missed_days = State()
+
+    waiting_for_cover = State()
+    waiting_for_cover_days = State()
+
+    waiting_for_active_students = State()
+    waiting_for_active_debtors = State()
+
+    waiting_for_archive_students = State()
+    waiting_for_archive_debtors = State()
