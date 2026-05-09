41a30f2706940cbe6bb3457e1f916efd34c4ee7a
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 19:52:55 +0500

    Update common_keyboard.py

--- keyboards/common_keyboard.py
+++ keyboards/common_keyboard.py
@@ -1,1 +1,10 @@
+from aiogram.types import ReplyKeyboardMarkup
 
+
+def home_keyboard():
+
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add("🏠 Bosh sahifa")
+
+    return keyboard
