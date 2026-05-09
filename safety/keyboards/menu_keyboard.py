1d57143583ef9d6a780f9388a7a950042288b52f
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 02:41:48 +0500

    Update menu_keyboard.py

--- safety/keyboards/menu_keyboard.py
+++ safety/keyboards/menu_keyboard.py
@@ -1,1 +1,29 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
+def get_menu(role):
+
+    keyboard = ReplyKeyboardMarkup(
+        resize_keyboard=True
+    )
+
+    if role == "manager":
+
+        keyboard.add("📊 Admin Salary")
+        keyboard.add("💰 Kassir Salary")
+        keyboard.add("👥 Users")
+
+    elif role == "admin":
+
+        keyboard.add("📊 Admin Salary")
+
+    elif role == "cashier":
+
+        keyboard.add("💰 Kassir Salary")
+
+    elif role == "coordinator":
+
+        keyboard.add("📊 Admin Salary")
+        keyboard.add("💰 Kassir Salary")
+
+    return keyboard
