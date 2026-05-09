df04cc3e8c482b656f2ac0e53209679e3fd29633
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 02:43:06 +0500

    Update main.py

--- safety/main.py
+++ safety/main.py
@@ -1,1 +1,17 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+from config import BOT_TOKEN
+
+
+bot = Bot(token=BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+import handlers.start
+import handlers.approvals
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
