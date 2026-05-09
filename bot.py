109fb0624ed0d68d3265b194239f996acb74cf2e
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 01:11:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
 
 from config import TOKEN
 
