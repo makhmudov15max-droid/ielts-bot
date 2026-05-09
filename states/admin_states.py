a04580e13f0478bdae602a5012cb82bc5ca2fcd4
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-09 19:52:38 +0500

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -1,18 +1,5 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup
 
-TOKEN = "8679587093:AAGjXpGVMiAexuNKPzRpQjASQRb8K2DYvyg"
-
-bot = Bot(token=TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
-
-# =====================================
-# STATES
-# =====================================
 
 class AdminSalaryStates(StatesGroup):
 
