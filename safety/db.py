6b268d1191b4edc076efc810e63a4d00cab96249
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 19:28:37 +0500

    Update db.py

--- safety/db.py
+++ safety/db.py
@@ -1,21 +1,25 @@
 import json
 
 
+USERS_FILE = "safety/database/users.json"
+PENDING_FILE = "safety/database/pending_users.json"
+
+
 def load_users():
-    with open("database/users.json", "r") as file:
+    with open(USERS_FILE, "r") as file:
         return json.load(file)
 
 
 def save_users(data):
-    with open("database/users.json", "w") as file:
+    with open(USERS_FILE, "w") as file:
         json.dump(data, file, indent=4)
 
 
 def load_pending():
-    with open("database/pending_users.json", "r") as file:
+    with open(PENDING_FILE, "r") as file:
         return json.load(file)
 
 
 def save_pending(data):
-    with open("database/pending_users.json", "w") as file:
+    with open(PENDING_FILE, "w") as file:
         json.dump(data, file, indent=4)
