f76782ec1f3f767be384faa7b86547573d0b3263
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-11 00:35:43 +0500

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -1,27 +1,13 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
+from aiogram.fsm.state import State, StatesGroup
 
 class AdminSalaryStates(StatesGroup):
+    fixed_salary = State()
 
-    waiting_for_status = State()
+    individual_kpi = State()
+    conversion_kpi = State()
+    active_kpi = State()
 
-    waiting_for_hours = State()
-    waiting_for_days = State()
+    bonus = State()
+    penalty = State()
 
-    waiting_for_individual_plan = State()
-    waiting_for_actual_sales = State()
-
-    waiting_for_conversion_plan = State()
-    waiting_for_actual_conversion = State()
-
-    waiting_for_active_plan = State()
-    waiting_for_actual_active = State()
-
-    waiting_for_russian = State()
-    waiting_for_ielts = State()
-
-    waiting_for_missed_work = State()
-    waiting_for_missed_hours = State()
-
-    waiting_for_cover = State()
-    waiting_for_cover_hours = State()
+    confirm = State()
