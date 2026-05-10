c8ec507962f58ba0b5d369521a5b74cf14eec766
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 17:54:54 +0500

    Update admin_salary.py

--- handlers/admin_salary.py
+++ handlers/admin_salary.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup
-from db import load_users
+from safety.db import load_users
 
 from keyboards.admin_keyboard import (
     main_menu_keyboard,
