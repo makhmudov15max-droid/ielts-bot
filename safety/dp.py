3255c8b5a4d7b504d1a8a8cd5a41d9336fce4102
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 17:41:42 +0500

    Update dp.py

--- safety/dp.py
+++ safety/dp.py
@@ -9,3 +9,13 @@ def load_users():
 def save_users(data):
     with open("database/users.json", "w") as file:
         json.dump(data, file, indent=4)
+
+
+def load_pending():
+    with open("database/pending_users.json", "r") as file:
+        return json.load(file)
+
+
+def save_pending(data):
+    with open("database/pending_users.json", "w") as file:
+        json.dump(data, file, indent=4)
