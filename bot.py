8a7dc2a60993fb51fd028610fb7d7751f97b966b
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 22:47:28 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,19 @@ from aiogram import executor
 
 from safety.loader import dp
 
-from handlers.admin_salary import register_admin_handlers
-from handlers.cashier_salary import register_cashier_handlers
+# IMPORT HANDLERS
 
-import safety.handlers.start
-import safety.handlers.approvals
+import handlers.start
+import handlers.approvals
+
+import handlers.admin_salary
+import handlers.cashier_salary
 
 
-register_admin_handlers(dp)
-register_cashier_handlers(dp)
-
+# START BOT
 
 if __name__ == "__main__":
+
     executor.start_polling(
         dp,
         skip_updates=True
