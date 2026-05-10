6ed62df6d84367376f9a84937d743e6f1801c744
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 18:30:09 +0500

    Update loader.py

--- safety/loader.py
+++ safety/loader.py
@@ -1,8 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from config import BOT_TOKEN
-
+from config import TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 
