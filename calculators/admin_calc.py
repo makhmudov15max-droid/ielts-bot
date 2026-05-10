2b2c15f74215161a335097843b42ba77c872a416
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-11 01:17:56 +0500

    Update admin_calc.py

--- calculators/admin_calc.py
+++ calculators/admin_calc.py
@@ -1,1 +1,24 @@
-# temp
+HOURLY_RATES = {
+    "nova": 11000,
+    "prime": 12000,
+    "apex": 13000,
+    "leader": 15000,
+}
+
+
+def calculate_admin_salary(data):
+
+    hourly_rate = HOURLY_RATES[data["status"]]
+
+    fixa = (
+        data["daily_hours"]
+        * data["worked_days"]
+        * hourly_rate
+    )
+
+    total_salary = fixa
+
+    return {
+        "fixa": int(fixa),
+        "total_salary": int(total_salary),
+    }
