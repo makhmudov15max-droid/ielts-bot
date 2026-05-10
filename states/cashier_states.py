cff4a4222e987eb0622bcaf50fa5c74537fb44d8
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-10 23:28:37 +0500

    Update cashier_states.py

--- states/cashier_states.py
+++ states/cashier_states.py
@@ -7,21 +7,19 @@ from aiogram.dispatcher.filters.state import (
 class CashierStates(StatesGroup):
 
     hours = State()
+    custom_hours = State()
 
     days = State()
+    custom_days = State()
 
     cover = State()
-
     cover_hours = State()
 
     absent = State()
-
     absent_hours = State()
 
     active_students = State()
-
     active_debtors = State()
 
     archive_students = State()
-
     archive_debtors = State()
