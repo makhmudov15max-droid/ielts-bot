9cc2528fa4e542483f9a6a00c787b65db678a1f9
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-11 01:07:15 +0500

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -1,25 +1,5 @@
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class AdminSalaryStates(StatesGroup):
-    status = State()
-
-    daily_hours = State()
-    worked_days = State()
-
-    individual_plan = State()
-    actual_sales = State()
-
-    conversion_plan = State()
-    actual_conversion = State()
-
-    active_plan = State()
-    actual_active = State()
-
-    knows_russian = State()
-    has_ielts = State()
-
-    cover_hours = State()
-    missed_hours = State()
-
-    confirm = State()
+    test = State()
