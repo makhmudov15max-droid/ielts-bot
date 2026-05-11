36e51fa510ae4b1065a84e3e5433ac4d08fd6e32
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-11 19:59:27 +0500

    Update admin_states.py

--- states/admin_states.py
+++ states/admin_states.py
@@ -4,12 +4,31 @@ from aiogram.dispatcher.filters.state import (
 )
 
 
-class AdminStates(StatesGroup):
+class AdminSalaryStates(StatesGroup):
 
     status = State()
 
-    hours = State()
+    daily_hours = State()
+    custom_daily_hours = State()
 
-    days = State()
+    worked_days = State()
+    custom_worked_days = State()
 
-    kpi = State()
+    has_ielts = State()
+
+    knows_russian = State()
+
+    missed = State()
+    missed_hours = State()
+
+    cover = State()
+    cover_hours = State()
+
+    individual_plan = State()
+    actual_sales = State()
+
+    conversion_plan = State()
+    actual_conversion = State()
+
+    active_plan = State()
+    actual_active = State()
