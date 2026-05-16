2fa315cc3f81d6fc2c48571e9d60f4367594b8b6
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 00:48:54 +0500

    Update states.py

--- Handlers/states.py
+++ Handlers/states.py
@@ -1,1 +1,6 @@
+from aiogram.fsm.state import StatesGroup, State
 
+# Task yaratish bosqichlari
+class TaskStates(StatesGroup):
+    waiting_for_name = State()  # Vazifa nomini kutish holati
+    waiting_for_days = State()  # Kunlarni tanlashni kutish holati
