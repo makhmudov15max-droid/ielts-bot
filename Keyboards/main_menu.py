7ed7cd197a195e10c9d8dde162f464e822d1ed97
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 00:09:18 +0500

    Update main_menu.py

--- Keyboards/main_menu.py
+++ Keyboards/main_menu.py
@@ -1,1 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+# "Vazifalar" tugmasini yaratamiz
+main_menu_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Vazifalar") # Tugma nomi
+        ]
+    ],
+    resize_keyboard=True # Tugma ekran o'lchamiga moslashishi uchun
+)
