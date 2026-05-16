47e0ccc7f59584992b5bdff8acb26a3407d04fca
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 00:37:37 +0500

    Update main_menu.py

--- Keyboards/main_menu.py
+++ Keyboards/main_menu.py
@@ -1,12 +1,23 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Yonma-yon turadigan tugmalar
+# Asosiy menyu tugmalari
 main_menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="Add Task"), 
             KeyboardButton(text="List of tasks")
         ] # Ikkalasi bitta qatorda
+    ],
+    resize_keyboard=True
+)
+
+# Task turini tanlash uchun yangi tugmalar (bitta qatorda yonma-yon)
+task_type_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Continuously"),
+            KeyboardButton(text="Daily")
+        ]
     ],
     resize_keyboard=True
 )
