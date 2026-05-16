57c65455e7d489c51950a6566f2c2087050a7fdf
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 00:40:22 +0500

    Update start.py

--- Handlers/start.py
+++ Handlers/start.py
@@ -1,16 +1,21 @@
-from aiogram import Router, types
+from aiogram import Router, types, F  # <--- F filtrini qo'shdik
 from aiogram.filters import CommandStart
-# Boya yaratgan tugmamizni bu yerga chaqirib (import qilib) olamiz
-from Keyboards.main_menu import main_menu_keyboard
+from Keyboards.main_menu import main_menu_keyboard, task_type_keyboard # <--- Yangi tugmani ham chaqirdik
 
-# Handlers (boshqaruvchilar) uchun Router yaratamiz
 start_router = Router()
 
-# Foydalanuvchi /start bosganida ishlaydigan funksiya
 @start_router.message(CommandStart())
 async def command_start_handler(message: types.Message):
     await message.answer(
         text=f"Salom, {message.from_user.full_name}! Botimizga xush kelibsiz.\n"
              f"Quyidagi tugma orqali vazifalarni ko'rishingiz mumkin 👇",
-        reply_markup=main_menu_keyboard # Boyagi tugmani xabarga biriktiramiz
+        reply_markup=main_menu_keyboard
+    )
+
+# "Add Task" tugmasi bosilganda ishlaydigan yangi qism
+@start_router.message(F.text == "Add Task")
+async def add_task_handler(message: types.Message):
+    await message.answer(
+        text="Qanday turdagi task yaratmoqchisiz?",
+        reply_markup=task_type_keyboard # Yangi Continuously va Daily tugmalarini ko'rsatamiz
     )
