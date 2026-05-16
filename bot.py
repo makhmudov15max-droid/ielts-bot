3a3d5e2dbb80bf058b5b3c7146a591679bec065a
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 00:14:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+# Handlers papkasidagi start.py ichidan routerni chaqirib olamiz
+from Handlers.start import start_router
 
+# Bot ishga tushganda terminalda ma'lumotlarni chiroyli ko'rsatib turishi uchun logging
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+async def main():
+    # Botni token orqali ro'yxatdan o'tkazamiz
+    bot = Bot(token=BOT_TOKEN)
+    
+    # Dispatcher - bu barcha routerlarni o'ziga jamlovchi asosiy markaz
+    dp = Dispatcher()
+    
+    # Boyagi start_router'ni asosiy dispatcherga ulaymiz
+    dp.include_router(start_router)
+    
+    # Botni yangi xabarlarni kutish rejimida ishga tushiramiz (Polling)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    # Loyihani ishga tushirish
+    asyncio.run(main())
