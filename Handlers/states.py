dc506a5191395127691f059089201c7960a6e9a1
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 01:57:01 +0500

    Update states.py

--- Handlers/states.py
+++ Handlers/states.py
@@ -4,6 +4,6 @@ from aiogram.fsm.state import StatesGroup, State
 class TaskStates(StatesGroup):
     waiting_for_name = State()            # Vazifa nomini kutish holati
     waiting_for_days = State()            # Kunlarni tanlashni kutish holati
-    waiting_for_frequency = State()       # Kunlik takrorlanish sonini kutish (Once/Multiple)
+    waiting_for_frequency = State()       # Kunlik takrorlanish sonini kutish holati
     waiting_for_once_time = State()       # Bitta aniq vaqtni matn ko'rinishida kutish (Yangi)
     waiting_for_multiple_times = State()  # Bir nechta vaqtni vergul bilan kutish (Yangi)
