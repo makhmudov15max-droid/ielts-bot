865e2464d03b4d7ae8c5cdd497aed136add092d1
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 00:29:51 +0500

    Update main_menu.py

--- Keyboards/main_menu.py
+++ Keyboards/main_menu.py
@@ -1,10 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Ustma-ust tushadigan tugmalar
+# Yonma-yon turadigan tugmalar
 main_menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Task qo'shish")], # 1-qator
-        [KeyboardButton(text="Tasklar ro'yxati")] # 2-qator
+        [
+            KeyboardButton(text="Tasklar ro'yxati"), 
+            KeyboardButton(text="Task yaratish")
+        ] # Ikkalasi bitta qatorda
     ],
     resize_keyboard=True
 )
