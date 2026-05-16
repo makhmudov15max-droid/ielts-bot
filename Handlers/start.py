522f7b179f0a50893ab7f48a5ecf8ddac15667b8
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 00:50:12 +0500

    Update start.py

--- Handlers/start.py
+++ Handlers/start.py
@@ -1,6 +1,8 @@
-from aiogram import Router, types, F  # <--- F filtrini qo'shdik
+from aiogram import Router, types, F
 from aiogram.filters import CommandStart
-from Keyboards.main_menu import main_menu_keyboard, task_type_keyboard # <--- Yangi tugmani ham chaqirdik
+from aiogram.fsm.context import FSMContext # FSM bilan ishlash uchun
+from Keyboards.main_menu import main_menu_keyboard, task_type_keyboard, days_keyboard
+from Handlers.states import TaskStates # Holatlarimizni chaqiramiz
 
 start_router = Router()
 
@@ -12,10 +14,33 @@ async def command_start_handler(message: types.Message):
         reply_markup=main_menu_keyboard
     )
 
-# "Add Task" tugmasi bosilganda ishlaydigan yangi qism
 @start_router.message(F.text == "Add Task")
 async def add_task_handler(message: types.Message):
     await message.answer(
         text="Qanday turdagi task yaratmoqchisiz?",
-        reply_markup=task_type_keyboard # Yangi Continuously va Daily tugmalarini ko'rsatamiz
+        reply_markup=task_type_keyboard
     )
+
+# 1. "Continuously" bosilganda nomini so'rash va holatni o'zgartirish
+@start_router.message(F.text == "Continuously")
+async def continuously_handler(message: types.Message, state: FSMContext):
+    await message.answer(
+        text="Vazifa nomini kiriting!",
+        reply_markup=types.ReplyKeyboardRemove() # Eski tugmalarni vaqtincha yopib turamiz
+    )
+    # Botga hozir foydalanuvchi nom kiritish bosqichida ekanligini aytamiz
+    await state.set_state(TaskStates.waiting_for_name)
+
+# 2. Foydalanuvchi nom kiritganida uni ushlab qolish
+@start_router.message(TaskStates.waiting_for_name)
+async def get_task_name_handler(message: types.Message, state: FSMContext):
+    # Kiritilgan nomni bot xotirasiga saqlaymiz (masalan: "Ishga keldim")
+    await state.update_data(task_name=message.text)
+    
+    # Keyingi savolga o'tamiz va yangi tugmalarni ko'rsatamiz
+    await message.answer(
+        text="Vazifa qaysi kunlari ko'rinsin?",
+        reply_markup=days_keyboard
+    )
+    # Holatni kunlarni kutish bosqichiga o'tkazamiz
+    await state.set_state(TaskStates.waiting_for_days)
