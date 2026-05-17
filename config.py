f04fb880036c5a7361d1a3ad0e8f15812f564f8d
makhmudov15max-droid <makhmudov15max@gmail.com>
2026-05-17 12:22:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ BOT_TOKEN = "8990910547:AAEB1wFrKkzx5D9XPT0ZvvEtZ-lxb6NKGw4"
 
 # Asosiy adminning telegram ID raqami (So'rovnomalar shu ID ga boradi)
 ADMIN_ID = 6500594896  #BM Telegram ID raqamlari
+
+REPORTS_GROUP_ID = -5226036627  # Nazoratchi Guruh ID raqami
